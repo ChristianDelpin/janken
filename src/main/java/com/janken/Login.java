3d0e16a5f1edb648e6007ea80b30801827234b92
ChristianDelpin <chrisakde@gmail.com>
2023-04-06 19:02:56 -0400

    stuff

---
+++ src/main/java/com/janken/Login.java
@@ -0,0 +1,18 @@
+package com.janken;
+
+
+import java.util.Scanner;
+
+public class Login {
+    static boolean login()
+    {
+        System.out.println("log in");
+
+        boolean loginSuccess = false;
+
+        //user temps
+        return loginSuccess;
+    } 
+
+    Scanner scanner = new Scanner(System.in);
+}
