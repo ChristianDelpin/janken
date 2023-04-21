67252c974a15597dd27e5379cd04f8845316ff66
ChristianDelpin <129323082+ChristianDelpin@users.noreply.github.com>
2023-04-21 15:10:01 -0400

    Finalized?

--- src/main/java/com/janken/Score.java
+++ src/main/java/com/janken/Score.java
@@ -3,50 +3,88 @@ package com.janken;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 
-public static class Score {
-    /**
-     * Method to get, update, and/or display the highest score.
-     * 
-     * @param isNewHighScore Boolean variable that will determine whether to overwrite the highscore file or to display the high score.
-     */
-    static void displayHighScore(boolean isNewHighScore)
+public class Score {
+
+
+  static Scanner scanner = new Scanner(System.in);
+  public static int currentScore;
+  public static int allTimeHighsore;
+  private static Path filePath;
+  
+    static void displayHighScore()
     {
+      System.out.println("**********\nAll-time High Score: "+allTimeHighsore+"\nCurrent Score: "+currentScore+"\n**********");
+    }
 
-        try {
-            File scoreTxt = new File("highscore.txt");
-            if (scoreTxt.exists()) 
-            {
-              //File exists. 
-
-              //Delete file if overwrite no worky??
-            } 
-            else 
-            {
-              //File does not exist.
-
-              scoreTxt.createNewFile();
-              
-              FileWriter writer = new FileWriter(scoreTxt.getName());
-              writer.write("");
-            }
-          } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+    static void setAllTimeHighscore()
+    {
+      try {
+        File scoreTxt = new File("highscore.txt");
+        if (scoreTxt.exists()) 
+        {
+          //File exists. 
+          filePath = scoreTxt.toPath();
+          String fileContents = new String(Files.readAllBytes(filePath));
+          try
+          {
+            String[] splitContent = fileContents.split("-");
+            allTimeHighsore = Integer.parseInt(splitContent[splitContent.length-1].trim());
           }
-
-        if(isNewHighScore)
+          catch(Exception e)
+          {
+            //File exists, but no content to split on.
+            allTimeHighsore = 0;
+          }
+        } 
+        else 
         {
-            //Updates high score and displays new score.
-            //System.out.println();
-
-            String name = scanner.nextLine();
-            
+          //File does not exist.
+          scoreTxt.createNewFile();
+          filePath = scoreTxt.toPath();
+          FileWriter writer = new FileWriter(scoreTxt.getName());
+          writer.write("");
+          filePath = scoreTxt.toPath();
+          allTimeHighsore = 0;
         }
+      } catch (IOException e) 
+      {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
+    }
+
+    static void newHighScoreReached()
+    {
+      //Format for txt file:
+      //Name - Score
+      //Ex:
+      //Bike - 4
+      System.out.println("Congrats, you have reached a new high score! Please enter your name below to register your new score!");
+      String name = scanner.nextLine();
+      String leaderboard = name + " - " + currentScore;
+      try 
+      {
+        Files.write(filePath, leaderboard.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+      } 
+      catch (IOException e) 
+      {
+        e.printStackTrace();
+      }
+      Events.log("🎉🎉New high score reached: "+leaderboard);
+    }
+
+    public static boolean isCurrentScoreNewHighScore()
+    {
+        boolean isNewHighScore;
+        if(currentScore>allTimeHighsore)
+            isNewHighScore=true;
         else
-        {
-            //Displays current high score
-        }
-    }    
+            isNewHighScore=false;
+        return isNewHighScore;
+    }
 }
